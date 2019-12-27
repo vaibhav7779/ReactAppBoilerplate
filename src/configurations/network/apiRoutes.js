4c9da18e0ac47354ac6daedcc946392be7c78e73
Ankit Gandhi <ankitgandhi@Ankits-MacBook-Pro.local>
2019-12-27 15:51:04 +0530

    Axios Setup at base

---
+++ src/configurations/network/apiRoutes.js
@@ -0,0 +1,6 @@
+const apiRoutes = {
+  baseURL: 'http://localhost:5000', // process.env.APP_API_BASE_URL,
+  authentication: {},
+};
+
+export default apiRoutes;
---
+++ src/utils/Api.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import defaultConfig from 'configurations/network/defaultConfig';
+import apiRoutes from 'configurations/network/apiRoutes';
+import SecureRequest from './SecureRequest';
+
+class Api {
+  static SecureRequest = SecureRequest();
+
+  constructor() {
+    const { baseURL } = apiRoutes;
+
+    this.authData = null;
+    this.requestManager = axios.create({
+      baseURL,
+      ...defaultConfig,
+    });
+
+    this.requestManager.interceptors.request.use(
+      Api.requestInterceptor,
+      Api.requestInterceptorError,
+    );
+    this.requestManager.interceptors.response.use(
+      Api.responseInterceptor,
+      Api.responseInterceptorError,
+    );
+  }
+
+  static requestInterceptor = config => {
+    const request = this.SecureRequest.getSecureAuthRequest(config);
+    return request;
+  };
+
+  static requestInterceptorError = error => {
+    return Promise.reject(error);
+  };
+
+  static responseInterceptor = response => {
+    const newResponse = this.SecureRequest.getResponse(response);
+    return newResponse;
+  };
+
+  static responseInterceptorError = error => {
+    return Promise.reject(error);
+  };
+
+  fetch = (options, action) => {
+    return this.requestManager.request(options);
+  };
+
+  errorHandling = (error, _action, _retries) => {
+    if (!error.status) return false;
+
+    if (error.status === 401) {
+      // ApiInstance logout method
+    }
+
+    return error.status >= 400 && error.status < 500;
+  };
+}
+
+export default Api;
+
+window.Api = Api;
