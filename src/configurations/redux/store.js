399a9fdb6562ece2f0339f2098b22f64f406dd09
ankit.gandhi@freecharge.com <ankit.gandhi@freecharge.com>
2021-07-30 00:34:06 +0530

    Updated Packages

--- src/configurations/redux/store.js
+++ src/configurations/redux/store.js
@@ -1,111 +1,111 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { persistStore, persistReducer } from 'redux-persist';
-import localForage from 'localforage';
-import Api from 'utils/Api';
-import createCompressor from 'redux-persist-transform-compress';
-import createEncryptor from 'redux-persist-transform-encrypt';
-import combinedReducer from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistStore, persistReducer } from 'redux-persist'
+import localForage from 'localforage'
+import Api from 'utils/Api'
+import createCompressor from 'redux-persist-transform-compress'
+import { encryptTransform } from 'redux-persist-transform-encrypt'
+import combinedReducer from './reducers'
 
-const persistStorage = localForage;
-const isPresistanceRequired = true;
-const apiInstance = new Api();
-const isDev = process.env.NODE_ENV === 'development';
-const compressor = createCompressor({});
-const encryptor = createEncryptor({
+const persistStorage = localForage
+const isPresistanceRequired = true
+const apiInstance = new Api()
+const isDev = process.env.NODE_ENV === 'development'
+const compressor = createCompressor({})
+const encryptor = encryptTransform({
   secretKey: 'ReactApp',
-  onError(error) {
+  onError (error) {
     // Handle the error.
-    console.log('Error while encryption', error);
-  },
-});
+    console.log('Error while encryption', error)
+  }
+})
 const persistConfig = {
   version: 0,
   key: 'ReactApp',
   storage: persistStorage,
   blacklist: [],
-  transforms: isDev ? [] : [compressor, encryptor],
-};
+  transforms: isDev ? [] : [compressor, encryptor]
+}
 
 const persistedReducer = isPresistanceRequired
   ? persistReducer(persistConfig, combinedReducer)
-  : combinedReducer;
+  : combinedReducer
 
 const middlewares = [
-  thunk.withExtraArgument({ apiInstance }), // Argument can be a request object used inside all calls
-];
+  thunk.withExtraArgument({ apiInstance }) // Argument can be a request object used inside all calls
+]
 
 const composeEnhancers = !isDev
   ? compose
   : composeWithDevTools({
-      // Specify name here, actionsBlacklist, actionsCreators and other options if needed
-    });
+    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+  })
 
 const store = createStore(
   persistedReducer,
-  composeEnhancers(applyMiddleware(...middlewares)),
-);
+  composeEnhancers(applyMiddleware(...middlewares))
+)
 
 const persistor = persistStore(store, {}, () => {
   // This is called fater rehydraete is done.
-  console.log('ReactApp store rehydrate done');
-});
+  console.log('ReactApp store rehydrate done')
+})
 
 const purgeData = () => {
   return new Promise((resolve, reject) => {
     persistor
       .purge()
       .then(res => {
-        resolve(true);
+        resolve(true)
       })
       .catch(err => {
-        reject(new Error(false));
-      });
-  });
-};
+        reject(new Error(false))
+      })
+  })
+}
 
 const flushData = () => {
   return new Promise((resolve, reject) => {
     persistor
       .flush()
       .then(res => {
-        resolve(true);
+        resolve(true)
       })
       .catch(err => {
-        reject(new Error(false));
-      });
-  });
-};
+        reject(new Error(false))
+      })
+  })
+}
 
 const pausePersistance = () => {
   return new Promise((resolve, reject) => {
     persistor
       .pause()
       .then(res => {
-        resolve(true);
+        resolve(true)
       })
       .catch(err => {
-        reject(new Error(false));
-      });
-  });
-};
+        reject(new Error(false))
+      })
+  })
+}
 
 const persistData = () => {
   return new Promise((resolve, reject) => {
     persistor
       .persist()
       .then(res => {
-        resolve(true);
+        resolve(true)
       })
       .catch(err => {
-        reject(new Error(false));
-      });
-  });
-};
+        reject(new Error(false))
+      })
+  })
+}
 
 if (isDev) {
-  window.persistor = persistor;
+  window.persistor = persistor
 }
 
 export {
@@ -114,5 +114,5 @@ export {
   purgeData,
   flushData,
   pausePersistance,
-  persistData,
-};
+  persistData
+}
